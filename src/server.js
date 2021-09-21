d4c3b9be5e863d955dccef1b4c13b0069a5b6eb5
declan <syas0301@gmail.com>
2021-09-22 02:33:28 +0900

    webRtc tutorial 1일차

--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/video", (req, res) => res.render("video"));
+//app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
